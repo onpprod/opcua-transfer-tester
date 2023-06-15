85851fdae68d7358fdc27a7012b7a9db6059fbd8
onpprod <otavionpproducoes@gmail.com>
2023-06-15 16:02:30 -0400

    finaltarde

---
+++ data.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+
+class Data(datetime):
+    def __init__(self,data = "01-01-2022 00:00:00",format = "%d-%m-%Y %H:%M:%S"):
+        super()
+
+    
+    def novaData():
+        
+        
